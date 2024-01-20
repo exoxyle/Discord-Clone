a0a28ad5da9be550f6c8852d20b677d5c2a4cc29
Sensei <130023589+pdlsensei@users.noreply.github.com>
2024-01-20 17:12:36 +0545

    darkmode

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import 
 
 const Home = () => {
   return (
-    <div className="flex flex-col mt-8">
-      <Button className="w-16 rounde" variant={"link"}>Add</Button>
-      <p className="text-red-600">Discord-Clone</p>
+    <div>
+      <UserButton 
+      afterSignOutUrl="/"
+      />
     </div>
   );
 };
